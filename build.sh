5f81ac3da3323f9a036c147c2239801bd8d43cd0
程序员晚枫 <1529577833@qq.com>
2022-11-11 01:03:28 +0800

    update linux

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd docs-pages/
-# npm run build
-/opt/software/node/node-v16.13.0-linux-x64/bin/yarn  build
+npm run build
+# /opt/software/node/node-v16.13.0-linux-x64/bin/yarn  build
 rm -rf /opt/website/python-office.com/*
 cp /opt/workplace/pro/python-office.com/dist/* /opt/website/python-office.com/ -R 
