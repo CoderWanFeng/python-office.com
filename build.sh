1906768bf7453a90afbb0fc09c4cb0dbe801f68d
CoderWanFeng <875143930@qq.com>
2023-08-20 01:44:25 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-nvm use 18.17.1
-yarn install
-yarn run build
+
 # /opt/software/node/node-v16.13.0-linux-x64/bin/yarn  build
 rm -rf /opt/website/python-office.com/*
 cp /opt/workplace/pro/python-office.com/dist/* /opt/website/python-office.com/ -R
