81b3bcf642be555c658cc4e3721e3aa95c052e94
CoderWanFeng <875143930@qq.com>
2023-08-19 23:56:52 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 cd docs-pages/
 npm run build
 # /opt/software/node/node-v16.13.0-linux-x64/bin/yarn  build
+mkdir /opt/website/python-office.com/
 rm -rf /opt/website/python-office.com/*
 cp /opt/workplace/pro/python-office.com/dist/* /opt/website/python-office.com/ -R 
