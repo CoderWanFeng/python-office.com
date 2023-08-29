b6863fd557ea0fa08212579176d60d0444e6551f
CoderWanFeng <875143930@qq.com>
2023-08-29 22:18:17 +0800

    增加：PDF加密👉https://www.bilibili.com/video/BV1H8411X7uu/

---
+++ docs-pages/vuepress/course/code/cooperator/03-pdf_jm.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/HYOWV7ImvTXImyYWtwADog
+@个人网站      ：www.python-office.com
+@代码日期    ：2023/8/29 22:01 
+@本段代码的视频说明     ：https://www.bilibili.com/video/BV1H8411X7uu/
+'''
+
+# 安装python
+# 安装python-office库
+# pip install python-office
+# ====================================================================
+
+# 导入python-office包
+import office
+
+# 将pdf文件加密
+# path：待加密pdf文件路径
+# password：输出pdf文件
+# output_path：输出pdf文件路径
+office.pdf.encrypt4pdf(
+    path=r"E:\program\python-office\pdf",
+    password="123@#",
+)
+
+# output_path=r"E:\program\python-office\encrypt"
+
+# 上面这种是Windows用户
+# 尊贵的Mac和Linux用户
+# ====================================================================
+# pip install popdf
+'''
+office.pdf.encrypt4pdf(
+    path=r"/program/python-office/pdf",
+    password="123@#",
+    output_path=r"/program/python-office/encrypt"
+)
+'''
--- docs-pages/vuepress/course/code/tools/video_add_mark.py
+++ docs-pages/vuepress/course/code/tools/video_add_mark.py
@@ -9,8 +9,8 @@
 
 import povideo
 
-povideo.mark2video(video_path=r'D:\迅雷下载\8月28日\8月28日.mp4',output_path=r'D:\迅雷下载\8月28日\output')
-# povideo.mark2video(video_path=r'D:\download\baiduyun\图片添加水印.mp4',output_path=r'D:\download\baiduyun\out')
+# povideo.mark2video(video_path=r'D:\迅雷下载\8月28日\8月28日.mp4',output_path=r'D:\迅雷下载\8月28日\output')
+povideo.mark2video(video_path=r'D:\download\baiduyun\0828白菜兔_5个实用的PDF自动化办公操作~1行Python代码搞定PDF加密.mp4',output_path=r'D:\download\baiduyun\out')
 import os
 IMAGEMAGICK_BINARY = os.getenv('IMAGEMAGICK_BINARY', 'auto-detect')
 # IMAGEMAGICK_BINARY = os.getenv('Path', 'auto-detect')
