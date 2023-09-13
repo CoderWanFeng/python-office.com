dedebbc1747cc63a6ccd8fbe27eac6b9fc6eb3dd
CoderWanFeng <875143930@qq.com>
2023-09-13 21:34:55 +0800

    发布16-18讲

---
+++ docs-pages/vuepress/course/code/50-16-down4img.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/HYOWV7ImvTXImyYWtwADog
+@个人网站      ：www.python-office.com
+@代码日期    ：2023/9/13 20:09 
+@本段代码的视频说明     ： 1行代码，下载图片
+'''
+import office
+
+office.image.down4img(url='https://python-office-1300615378.cos.ap-chongqing.myqcloud.com/icon2.jpg',
+                      output_path=r'D:\workplace\code\gitee\python-office.com\docs-pages\vuepress\course\code\test_files\50-16-down4img')
---
+++ docs-pages/vuepress/course/code/50-18-weather_robot.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/HYOWV7ImvTXImyYWtwADog
+@个人网站      ：www.python-office.com
+@代码日期    ：2023/9/13 20:26 
+@本段代码的视频说明     ：
+'''
+import office
+
+keywords = {
+    "生成密码": "你的密码是：" + office.tools.passwordtools(),
+    "点赞了吗？": "点了，我还打开了你的网站呢：python-office.com",
+    "关注了吗？": "必须的",
+    "投币了吗？": "三连走起",
+}
+
+office.wechat.chat_by_keywords(who='每天进步一点点', keywords=keywords)
